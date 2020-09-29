440fa8d1ec2ad3374a2a104f06843dd912521b85
gurapomu <gurapomu@gmail.com>
2020-09-29 17:31:21 +0900

    fix path

--- entry_point.sh
+++ entry_point.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-mkdir /host/tmp/node-exporter
+mkdir -p /metrics/node-exporter
 while true;
 do
   met="node_hwmon_temp_celsius"  #出力するメトリクス名
-  dir="/host/tmp/node-exporter/"   #出力ディレクトリ
+  dir="/metrics/node-exporter/"   #出力ディレクトリ
   ctemp=$( cat /sys/class/thermal/thermal_zone0/temp )  #CPU温度取得
   otempa="`expr $ctemp / 1000`"
   otempb="`expr $ctemp % 1000`"
