ff891449ad3ec703c017c77041431aa1159d37e0
gurapomu <gurapomu@gmail.com>
2020-09-29 17:02:53 +0900

    fix bug

--- entry_point.sh
+++ entry_point.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
-met="node_hwmon_temp_celsius"  #出力するメトリクス名
-dir="/tmp/node-exporter/"   #出力ディレクトリ
-ctemp=$( cat /sys/class/thermal/thermal_zone0/temp )  #CPU温度取得
-otempa="`expr $ctemp / 1000`"
-otempb="`expr $ctemp % 1000`"
-otemp="${otempa}.${otempb}"
-line1="# HELP ${met} CPU temperature"
-line2="# TYPE ${met} gauge"
-mkdir /tmp/node-exporter
 while true;
 do
+  met="node_hwmon_temp_celsius"  #出力するメトリクス名
+  dir="/tmp/node-exporter/"   #出力ディレクトリ
+  ctemp=$( cat /sys/class/thermal/thermal_zone0/temp )  #CPU温度取得
+  otempa="`expr $ctemp / 1000`"
+  otempb="`expr $ctemp % 1000`"
+  otemp="${otempa}.${otempb}"
+  line1="# HELP ${met} CPU temperature"
+  line2="# TYPE ${met} gauge"
+  mkdir /tmp/node-exporter
   /bin/echo "${line1}" > ${dir}${met}.prom.$$
   /bin/echo "${line2}" >> ${dir}${met}.prom.$$
   /bin/echo "${met} ${otemp}" >> ${dir}${met}.prom.$$
